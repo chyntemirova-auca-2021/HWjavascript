07261ff8954bf548dd57ee21fe6e1a7c9c7c2cda
Ramilia Chyntemirova <chyntemirova_r@auca.kg>
2021-12-10 13:21:12 +0600

    init

---
+++ hw27.js
@@ -0,0 +1,6 @@
+let a = Number(prompt("Side 1 :"));
+let b = Number(prompt("Side 2 :"));
+let c = Number(prompt("Side 3 :"));
+let d = Number(prompt("Side 4 :"));
+let s = (a + b + c + d) ;
+console.log(s);
