f81036c23f3a28998e7f68a8533e93fc24d8b6a0
Ramilia Chyntemirova <chyntemirova_r@auca.kg>
2022-02-12 00:16:11 +0600

    added hw 29

---
+++ hw29/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", run)
+
+function run () {
+    const btn = document.getElementById("plusButton")
+    btn.addEventListener("click", mySum)
+}
+
+    function mySum(){
+        const numberOne = document.querySelector("#input1")
+        const numberTwo = document.querySelector("#input2")
+        let result = Number (numberOne.value) + Number(numberTwo.value)
+        const resultNode = document.getElementById("myRes")
+        resultNode.textContent = result
+    }
